b4330cd832e24f7a233e3e892a8faae9611df536
Jaideep Singh <jaideep.solanki99@gmail.com>
2023-02-23 22:33:05 +0530

    Updating code

--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -1,12 +1,35 @@
 import React from "react";
 import "./LandingPage.css";
-import { Container, Row } from "react-bootstrap";
+import { Button, Container, Row } from "react-bootstrap";
 
 const LandingPage = () => {
   return (
     <div className="main">
       <Container>
-        <Row>Landing</Row>
+        <Row>
+          <div className="intro-text">
+            <div>
+              <h1 className="title">Welcome to Note Zipper</h1>
+              <p className="subtitle"> One safe place for all your notes</p>
+            </div>
+            <div className="buttonContainer">
+              <a href="/login">
+                <Button size="large" className="landingbutton">
+                  Login
+                </Button>
+              </a>
+              <a href="/login">
+                <Button
+                  size="large"
+                  className="landingbutton"
+                  variant="outline-primary"
+                >
+                  SignUp
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
       </Container>
     </div>
   );
